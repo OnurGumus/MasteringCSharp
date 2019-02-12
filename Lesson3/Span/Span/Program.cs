6e3eea73cf8edd0299677cd2628421dccc83c154
Onur Gumus <Onur.Gumus@emaratech.ae>
2019-02-12 19:02:42 +0400

    Span example

---
+++ Lesson3/Span/Span/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Span
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[100];
+            Span<int> span =  arr.AsSpan();
+            DoWork(span);
+            DoWorkM(arr.AsMemory(1,2)).Wait();
+
+            Span<int> span2 = stackalloc int[100];
+            IntPtr unmanagedHandle = Marshal.AllocHGlobal(256);
+            //works in unsafe context
+            //Span<byte> unmanaged = new Span<byte>(unmanagedHandle.ToPointer(), 256);
+            var streamWriter = new StreamWriter("path");
+            var s = "foo".AsSpan();
+            streamWriter.Write(s);
+        }
+
+        public static void DoWork(Span<int> items)
+        {
+
+        }
+
+        //public static async Task DoWork(Span<int> items)
+        //{
+
+        //}
+        public static async Task DoWorkM(Memory<int> items)
+        {
+
+        }
+    }
+}
